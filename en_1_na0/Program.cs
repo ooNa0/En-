c219622c50ded3981db89c7ba08017ba7886a19e
Na0 <koonayoung51@naver.com>
2021-04-05 20:50:03 +0900

    Main에서 switch 문 완성 + 입력값 받기

--- en_1_na0/Program.cs
+++ en_1_na0/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 
 namespace en_1_na0
 {
     class PrintInvertedStar
     {
-        int LineNum;
-        public PrintInvertedStar(int LineNum)
+        public PrintInvertedStar(int LineNumber)
         {
 
-            for (int i = LineNum; i > 0; i--)
+            for (int i = LineNumber; i > 0; i--)
             {
-                for (int j = LineNum; j > i; j--)
+                for (int j = LineNumber; j > i; j--)
                 {
                     Console.Write(" ");
                 }
@@ -25,12 +25,12 @@ namespace en_1_na0
     }
     class PrintStar
     {
-        int LineNum;
-        public PrintStar(int LineNum)
+        //int LineNum;
+        public PrintStar(int LineNumber)
         {
-            for (int i = 0; i < LineNum; i++)
+            for (int i = 0; i < LineNumber; i++)
             {
-                for (int j = LineNum; j > i; j--)
+                for (int j = LineNumber; j > i; j--)
                 {
                     Console.Write(" ");
                 }
@@ -52,20 +52,52 @@ namespace en_1_na0
             Console.WriteLine("*         2. PrintInvertedTriangleStar       *");
             Console.WriteLine("*         3. PrintHourglassStar              *");
             Console.WriteLine("*         4. PrintDiamondStar                *");
+            Console.WriteLine("*         5. Ending~~~~~~~~~~                *");
             Console.WriteLine("*                                            *");
             Console.WriteLine("==============================================");
-            Console.WriteLine("* Please Input Number");
+            Console.Write("* Please Input Number :");
         }
         static void Main(string[] args)
         {
+            bool count = true;
+            while (count)
+            {
+                ShowMenu(); // 메뉴 출력
+                string stringNumber;
+                int number;
+                stringNumber = Console.ReadLine();
+                number = Convert.ToInt32(stringNumber);
 
-            ShowMenu(); // 메뉴 출력
-            string stringNumber;
-            int number;
-            stringNumber = Console.ReadLine();
-            number = Convert.ToInt32(stringNumber);
+                Console.Write("* Please Input LineNumber :");
+                string stringLineNumber;
+                int LineNumber;
+                stringLineNumber = Console.ReadLine();
+                LineNumber = Convert.ToInt32(stringLineNumber);
 
-            
+                switch (number)
+                {
+                    case 1:
+                        PrintStar a = new PrintStar(LineNumber);
+                        break;
+                    case 2:
+                        PrintInvertedStar b = new PrintInvertedStar(LineNumber);
+                        break;
+                    case 3:
+                        PrintStar c = new PrintStar(LineNumber);
+                        PrintInvertedStar d = new PrintInvertedStar(LineNumber);
+                        break;
+                    case 4:
+                        PrintInvertedStar e = new PrintInvertedStar(LineNumber);
+                        PrintStar f = new PrintStar(LineNumber);
+                        break;
+                    case 5:
+                        count = false;
+                        break;
+                    default:
+                        Console.WriteLine("Oh,,, please select the number on the menu");
+                        break;
+                }
+            }
         }
     }
 }

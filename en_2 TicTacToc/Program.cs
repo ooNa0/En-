8aa39c6abe7708b72dd20b58938f74f082474ed5
Na0 <koonayoung51@naver.com>
2021-04-07 16:10:14 +0900

    [ADD} StartGame 메소드 생성

--- en_2 TicTacToc/Program.cs
+++ en_2 TicTacToc/Program.cs
@@ -6,7 +6,15 @@ namespace en_2_TicTacToc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Title = "TicTacToe";
+            //Console.SetWindowSize(95, 30);
+
+            // Creates and initializes a new ArrayList.
+            ArrayList _userScore = new ArrayList();
+
+            TicTacToe ticTacToe = new TicTacToe; // 틱택토 객체 실행
+            ticTacToe.StartGame(); // 틱택토 내 메소드 실행
         }
     }
 }
--- en_2 TicTacToc/TicTacToe.cs
+++ en_2 TicTacToc/TicTacToe.cs
@@ -6,5 +6,16 @@ namespace en_2_TicTacToc
 {
     class TicTacToe
     {
+        // 변수 선언
+        private int _number;
+        private bool _isFinished = false;
+        public void StartGame()
+        {
+            while (!_isFinished)
+            {
+
+            }
+
+        }
     }
 }

61dae4fe43f1961dc6f424fbfcb97bd249cb5c42
Na0 <koonayoung51@naver.com>
2021-04-07 15:56:01 +0900

    [ADD] TicTacToe 클래스 생성

---
+++ en_2 TicTacToc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace en_2_TicTacToc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ en_2 TicTacToc/TicTacToe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace en_2_TicTacToc
+{
+    class TicTacToe
+    {
+    }
+}

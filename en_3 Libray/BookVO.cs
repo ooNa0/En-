3bfad4fcd89de5d8d8bad8efc48ea6ff0f3f6fee
Na0 <koonayoung51@naver.com>
2021-04-11 02:42:25 +0900

    [ADD] BookVO override

--- en_3 Libray/BookVO.cs
+++ en_3 Libray/BookVO.cs
@@ -6,14 +6,14 @@ namespace en_3_Libray
 {
     class BookVO
     {
-        private int id;        // 도서 번호
+        private string id;        // 도서 번호
         private string bookName;      // 도서명
         private string publisher; // 출판사명
         private string author;    // 저자명
-        private int price;     // 가격
-        private int quantity;  // 수량
+        private string price;     // 가격
+        private string quantity;  // 수량
 
-        public BookVO(int id, string bookName, string publisher, string author, int price, int quantity)
+        public BookVO(string id, string bookName, string publisher, string author, string price, string quantity)
         {
             this.id = id;
             this.bookName = bookName;
@@ -22,7 +22,7 @@ namespace en_3_Libray
             this.quantity = quantity;
         }
 
-        public BookVO(int id, string bookName, string publisher) 
+        public BookVO(string id, string bookName, string publisher) 
             // 생성자 - 부모 클래스가 자식 클래스의 기반, 코드의 재활용
             // 상속 - 초기화 값 많고, 생설자가 필요할 경우
             //:this(id, bookName, publisher, "작가 미상")
@@ -31,7 +31,7 @@ namespace en_3_Libray
         }
 
         // VO get/set method, ToString()
-        public int Id
+        public string Id
         {
             get { return id; }
             set { id = value; }
@@ -46,6 +46,10 @@ namespace en_3_Libray
         {
             get { return publisher; }
             set { publisher = value; }
+        }
+        public override string ToString()
+        {
+            return "Book id: " + id + ", Name: " + BookName + ", Author: " + author;//base.ToString();
         }
     }
 }

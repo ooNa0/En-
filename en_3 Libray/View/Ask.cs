dd2ab330a36c16679e2968e0010388a5360099e0
Na0 <koonayoung51@naver.com>
2021-04-12 02:21:46 +0900

    Revert "[ADD] Ask 클래스에 askYesOrNo 메소드 추가"
    This reverts commit 81999ba03b3cb55c24c0a4ce7f048b9a5ec17da6.

--- en_3 Libray/View/Ask.cs
+++ en_3 Libray/View/Ask.cs
@@ -6,30 +6,5 @@ namespace en_3_Libray.View
 {
     class Ask
     {
-        public int AskYesOrNo()
-        {
-            Console.WriteLine("\"Yes\"나 \"No\"를 입력해주세요.");
-            bool isFinished = true;
-
-            while (isFinished)
-            {
-                string answer = Console.ReadLine();
-
-                switch (answer)
-                {
-                    case "Yes":
-                        return 0;
-                    case "No":
-                        return 1;
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("\"Yes\" 아니면 \"No\"만 입력이 가능합니다..!");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        continue;
-                }
-            }
-            return 0;
-
-        }
     }
 }

b72b0383d7fa7bd7eb92dcbf06374ff674796a97
Na0 <koonayoung51@naver.com>
2021-04-12 02:12:33 +0900

    [ADD] AdministratorMode 메소드 생성

--- en_3 Libray/View/Printing.cs
+++ en_3 Libray/View/Printing.cs
@@ -8,10 +8,20 @@ namespace en_3_Libray.View
     {
         public void ShowMenu()
         {
-            Console.WriteLine("1. 로그인");
-            Console.WriteLine("2. 회원가입");
-            Console.WriteLine("3. 관리자 모드");
-            Console.WriteLine("4. 종료하기");
+            Console.WriteLine("▷ 로그인");
+            Console.WriteLine("▷ 회원가입");
+            Console.WriteLine("▷ 관리자 모드");
+            Console.WriteLine("▷ 종료하기");
+        }
+
+        public void AdministratorMode()
+        {
+            Console.WriteLine("▷ 도서 등록");
+            Console.WriteLine("▷ 도서 정보 수정");
+            Console.WriteLine("▷ 도서 삭제");
+            Console.WriteLine("▷ 도서 검색");
+            Console.WriteLine("▷ 전체 도서 출력");
+            Console.WriteLine("▷ 돌아가기");
         }
     }
 }

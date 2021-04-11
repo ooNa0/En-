b88b5b02d0c33f6e594efa0bad5abaaa6e7557bc
Na0 <koonayoung51@naver.com>
2021-04-11 18:16:00 +0900

    [ADD] ShowMenu 메소드 생성

--- en_3 Libray/View/Printing.cs
+++ en_3 Libray/View/Printing.cs
@@ -6,5 +6,12 @@ namespace en_3_Libray.View
 {
     class Printing
     {
+        public void ShowMenu()
+        {
+            Console.WriteLine("1. 로그인");
+            Console.WriteLine("2. 회원가입");
+            Console.WriteLine("3. 관리자 모드");
+            Console.WriteLine("4. 종료하기");
+        }
     }
 }

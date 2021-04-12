8dc977a4e66919f49abcf28a5bb992847459537c
Na0 <koonayoung51@naver.com>
2021-04-12 22:54:44 +0900

    [FIX] Login 클래스 수정

--- en_3 Libray/User/Login.cs
+++ en_3 Libray/User/Login.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using en_3_Libray.Data;
+using en_3_Libray.User;
+using en_3_Libray.View;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -10,21 +14,77 @@ namespace en_3_Libray.Exception
     class Login
     {
         public string _identity;
-        public string _passWord ="";
-
-        public void StartLogin()
+        public string _passWord = "";
+        public void StartLogin(List<UserVO> user)
         {
             int failCount = 0;
             bool isSet = true;
-            while (isSet)
+            Menu menu = new Menu();
+            SignUp signup = new SignUp();
+            int count = user.Count - 1;
+
+            if (count < 0)
             {
                 Console.Clear();
-                Console.Write("(영어와 숫자로 이루어진 10자리 이하)\n아이디 : ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("회원가입을 먼저해주세요.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("아무키나 입력해주세요..");
+                Console.ReadLine();
+                menu.MainMenu(user);
+            }
+
+            while (isSet) // 아이디
+            {
+                Console.Clear();
+                Console.WriteLine(user.Count);
+                Console.Write("아이디 :");
+
                 _identity = Console.ReadLine();
-                if (!Regex.IsMatch(_identity, "^[a-zA-Z0-9]{1,10}$"))
+
+                Console.WriteLine(user.Count);
+
+                while (count >= 0)
                 {
-                    if (failCount > 5) { Console.WriteLine("아이디는 영어와 숫자로 이루어진 10자리 이하 문자열입니다."); }
-                    else if(failCount > 10)
+                    if (_identity != user[count].Id)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("이미 존재하는 아이디가 아닙니다.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("아무키나 입력해주세요..");
+                        Console.ReadLine();
+                        menu.MainMenu();
+                        //continue;
+                    }
+                    else
+                    {
+                        isSet = false;
+                        break;
+                    }
+                }
+            }
+
+            count = user.Count - 1;
+            while (!isSet)
+            {
+                ConsoleKeyInfo keyInput;
+                //Console.Clear();
+                Console.WriteLine("아이디 :" + _identity);
+                Console.Write("비밀번호 : ");
+                keyInput = Console.ReadKey(true);
+                _passWord = "";
+                while (keyInput.Key != ConsoleKey.Enter)
+                {
+                    _passWord += keyInput.KeyChar;
+                    Console.Write("*");
+                    keyInput = Console.ReadKey(true);
+                }
+                Console.Write(_passWord);
+
+                if (_passWord != user[count].PassWord)//// 해당 아이디에 비밀번호가 맞는지
+                {
+                    Console.WriteLine("\n틀렸습니다. 다시 입력해주세요. (5번 이상 틀릴 경우 초기화면으로 돌아갑니다.");
+                    if (failCount > 5)
                     {
                         Console.WriteLine(" 초기 화면으로 돌아갑니다.");
                         Thread.Sleep(500);
@@ -33,35 +93,56 @@ namespace en_3_Libray.Exception
                     }
                     Console.ReadLine();
                     failCount++;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            while (isSet)
-            {
-                Console.Write("비밀번호 : ");
-                ConsoleKeyInfo keyInfo;
-                keyInfo = Console.ReadKey(true);
-                //keyInfo.Key != ConsoleKey.Backspace && 
-                while (keyInfo.Key != ConsoleKey.Enter) {
-                    _passWord += keyInfo.KeyChar;
-                    Console.Write("*");
-                    keyInfo = Console.ReadKey(true);
-                }
-                if(_passWord.Length < 4 || _passWord.Length > 15)//// 해당 아이디에 비밀번호가 맞는지
-                {
-                    Console.WriteLine("\n틀렸습니다. 다시 입력해주세요.");
-                    Console.ReadLine();
-                    Console.Clear();
+                    //Console.Clear();
                     Console.Write("아이디 : " + _identity + "\n");
                 }
                 else
                 {
-                    isSet = false;
+                    Console.WriteLine("로그인 되었습니다. 반갑습니다. " + user[count].Name + "님");
+                    Thread.Sleep(1000);
+                    isSet = true;
                 }
+
             }
         }
     }
 }
+/*
+if (!(Regex.IsMatch(_identity, "^[a-zA-Z0-9]{1,10}$"))) // 영어와 숫자만 받음
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("영어(대소문자 상관 없음)와 숫자만 10자리 이내로 입력해주세요.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("아무키나 입력해주세요..");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    isSet = false;
+                    //user[userNumber].Id = _identity;
+                }
+
+                while (isSet)
+                {
+                    Console.Clear();
+                    Console.Write("아이디 : ");//(영어와 숫자로 이루어진 10자리 이하)\n
+                    _identity = Console.ReadLine();
+                    if (!Regex.IsMatch(_identity, "^[a-zA-Z0-9]{1,10}$"))
+                    {
+                        if (failCount > 5) { Console.WriteLine("아이디는 영어와 숫자로 이루어진 10자리 이하 문자열입니다."); }
+                        else if (failCount > 10)
+                        {
+                            Console.WriteLine(" 초기 화면으로 돌아갑니다.");
+                            Thread.Sleep(500);
+                            Console.ReadLine();
+                            return;
+                        }
+                        Console.ReadLine();
+                        failCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+*/

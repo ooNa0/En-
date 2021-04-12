8581ceb556f85d9b9db72503e7cb94529e6d8ae7
Na0 <koonayoung51@naver.com>
2021-04-12 16:45:05 +0900

    [ADD] Ask 클래스 로그 누적 수정

--- en_3 Libray/View/Ask.cs
+++ en_3 Libray/View/Ask.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace en_3_Libray.View
 {
     class Ask
     {
+        Menu menu = new Menu();
         public int AskYesOrNo()
         {
             Console.WriteLine("\"Yes\"나 \"No\"를 입력해주세요.");
             bool isFinished = true;
+            ConsoleKeyInfo keyInput;
 
             while (isFinished)
             {
-                string answer = Console.ReadLine();
-
+                string answer = "";
+                keyInput = Console.ReadKey(false);
+                while (keyInput.Key != ConsoleKey.Enter)
+                {
+                    if (keyInput.Key == ConsoleKey.Escape)
+                    {
+                        menu.MainMenu();
+                    }
+                    answer += keyInput;
+                    keyInput = Console.ReadKey(false);
+                }
+                
                 switch (answer)
                 {
                     case "Yes":
@@ -25,6 +38,10 @@ namespace en_3_Libray.View
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("\"Yes\" 아니면 \"No\"만 입력이 가능합니다..!");
                         Console.ForegroundColor = ConsoleColor.White;
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        Console.WriteLine("정말로 프로그램을 종료하시겠어요?");
+                        Console.WriteLine("\"Yes\"나 \"No\"를 입력해주세요.");
                         continue;
                 }
             }

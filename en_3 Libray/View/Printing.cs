5b03c9a4cf9905fd23e82267a8035960fbc92263
Na0 <koonayoung51@naver.com>
2021-04-12 13:41:21 +0900

    [ADD] UserMenu 추가

--- en_3 Libray/View/Printing.cs
+++ en_3 Libray/View/Printing.cs
@@ -11,7 +11,18 @@ namespace en_3_Libray.View
             Console.WriteLine("▷ 로그인");
             Console.WriteLine("▷ 회원가입");
             Console.WriteLine("▷ 관리자 모드");
-            Console.WriteLine("▷ 종료하기");
+            Console.WriteLine("▷ 종료");
+        }
+
+        public void UserMenu()
+        {
+            Console.WriteLine("▷ 도서 검색");
+            Console.WriteLine("▷ 도서 대출");
+            Console.WriteLine("▷ 도서 반납");
+            Console.WriteLine("▷ 전체 도서 출력");
+            Console.WriteLine("▷ 나의 정보");
+            Console.WriteLine("▷ 종료");
+            Console.WriteLine("▷ 로그아웃");
         }
 
         public void AdministratorMode()
@@ -21,6 +32,9 @@ namespace en_3_Libray.View
             Console.WriteLine("▷ 도서 삭제");
             Console.WriteLine("▷ 도서 검색");
             Console.WriteLine("▷ 전체 도서 출력");
+            Console.WriteLine("▷ 회원 리스트");
+            Console.WriteLine("▷ 회원 검색");
+            Console.WriteLine("▷ 회원 삭제");
             Console.WriteLine("▷ 돌아가기");
         }
     }

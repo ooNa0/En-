1abf10a4463f0e2a89b40908ea03d6101e188f0d
Na0 <koonayoung51@naver.com>
2021-04-12 22:39:42 +0900

    [FIX] UserVO 수정

--- en_3 Libray/Data/UserVO.cs
+++ en_3 Libray/Data/UserVO.cs
@@ -6,15 +6,43 @@ namespace en_3_Libray.Data
 {
     class UserVO
     {
-        public string Id { get; set; } // 아이디
-        public string PassWord { get; set; } // 비밀번호
-        public string Name { get; set; } // 이름
-        public string Age { get; set; } // 나이
-        public string PhoneNumber { get; set; } // 전화번호
+        private string id;
+        private string password;
+        private string name;
+        private string age;
+        private string phoneNumber;
 
+        
+        public string Id // 아이디
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string PassWord // 비밀번호
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public string Name // 이름
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Age // 나이
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string PhoneNumber // 전화번호
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
         public override string ToString()
         {
-            return "이름: " + Name.PadRight(4, ' ') + "아이디: " + Id.PadRight(10, ' ') + "나이: " + Age.PadRight(3, ' ') + "전화번호: " + PhoneNumber;
+            return "이름: " + Name + "아이디: " + Id + "나이: " + Age + "전화번호: " + PhoneNumber;
+            //return "이름: " + Name.PadRight(4, ' ') + "아이디: " + Id.PadRight(10, ' ') + "나이: " + Age.PadRight(3, ' ') + "전화번호: " + PhoneNumber;
         }
     }
 }
+

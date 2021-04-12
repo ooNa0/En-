2897393073b0ab4e2b16b1557ef0cf3d7cd7f00b
Na0 <koonayoung51@naver.com>
2021-04-13 00:34:58 +0900

    [ADD] CerserControl 클래스 생성

---
+++ en_3 Libray/View/CerserControl.cs
@@ -0,0 +1,43 @@
+﻿using en_3_Libray.View;
+using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace en_3_Libray
+{
+    class CerserControl
+    {
+        public bool cerserControl(int Y, int min, int max)
+        {
+            Printing print = new Printing();
+            bool isOpen = false;
+            ConsoleKeyInfo cki;
+            int x = 0, y = 0;
+            while (!isOpen)
+            {
+                Console.Clear();
+                print.ShowMenu();
+                Console.SetCursorPosition(x, y);
+                Console.Write('▶');
+                cki = Console.ReadKey(true);
+                switch (cki.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (y > 0) { y--; }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (y < 3) { y++; }
+                        break;
+                    case ConsoleKey.Enter:
+                        SelectMenu(y);
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.WriteLine("메뉴로 돌아갑니다.");
+                        break;
+                }
+            }
+        }
+
+    }
+}

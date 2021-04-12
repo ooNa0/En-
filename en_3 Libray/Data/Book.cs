6e28100ceef31902a94154cad833d778677f75e9
Na0 <koonayoung51@naver.com>
2021-04-12 15:55:07 +0900

    [FIX] 클래스 auto 변경

--- en_3 Libray/Data/Book.cs
+++ en_3 Libray/Data/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace en_3_Libray
@@ -11,22 +12,10 @@ namespace en_3_Libray
         private string publisher; // 출판사명
         private string author;    // 저자명
         private string quantity;  // 수량
-
-        public Book(string id, string bookName, string publisher, string author, string quantity)
-        {
-            this.id = id;
-            this.bookName = bookName;
-            this.publisher = publisher;
-            this.quantity = quantity;
-        }
-
-        public Book(string id, string bookName, string publisher) 
-            // 생성자 - 부모 클래스가 자식 클래스의 기반, 코드의 재활용
-            // 상속 - 초기화 값 많고, 생설자가 필요할 경우
-            //:this(id, bookName, publisher, "작가 미상")
-        { 
-
-        }
+        private bool isBorrowed; // 대출중?
+        private string userId; // 대출중이면 누가
+        private DateTime borrowDate; // 대출일
+        private DateTime returnDate; // 반납일
 
         // VO get/set method, ToString()
         public string Id
@@ -45,9 +34,36 @@ namespace en_3_Libray
             get { return publisher; }
             set { publisher = value; }
         }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
         public override string ToString()
         {
             return "Book id: " + id + ", Name: " + BookName + ", Author: " + author;//base.ToString();
+        }
+
+        public Book(string id, string bookName, string publisher, string author, string quantity)
+        {
+            this.id = id;
+            this.bookName = bookName;
+            this.publisher = publisher;
+            this.author = author; 
+            this.quantity = quantity;
+        }
+
+        public Book(string id, string bookName, string publisher)
+        // 생성자 - 부모 클래스가 자식 클래스의 기반, 코드의 재활용
+        // 상속 - 초기화 값 많고, 생설자가 필요할 경우
+        //:this(id, bookName, publisher, "작가 미상")
+        {
+
         }
     }
 }

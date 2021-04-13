eb983c03f3906fc585ef8e2d0ee88b8013039fe8
Na0 <koonayoung51@naver.com>
2021-04-14 00:05:44 +0900

    [ADD] Constant 클래스 생성

---
+++ en_4 Library/Constant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace en_4_Library
+{
+    class Constant
+    {
+        public const int LOADING_TYPE_ONE = 1;
+
+        public const int LOADING_TYPE_TWO = 2;
+
+        public const int CONSOLE_SIZE_X = 80;
+
+        public const int CONSOLE_SIZE_X_IN_LECTURE_SEARCH_MENU = 190;
+
+        public const int CONSOLE_SIZE_Y = 45;
+
+        public const int Y_POSITION_BELLOW_BANNER = 28;
+
+        public const int TIME_TABLE_SIZE_X = 6;
+
+        public const int TIME_TABLE_SIZE_Y = 25;
+
+        public const int IS_PASSWORD = 1;
+
+
+        public const int SEARCH_BOOK_NAME = 0;
+
+        public const int SEARCH_BOOK_PUBLISHER = 1;
+
+        public const int SEARCH_BOOK_AUTHOR = 2;
+
+
+        public const int BORROW_BOOK = 0;
+
+        public const int RETURN_BOOK = 1;
+
+        public const int PRINT_ = 1;
+    }
+}
+
+// 입력 함수
+// 비밀번호 -> 별 입력함수 플래그 ispassword

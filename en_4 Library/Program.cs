20b6eabc6fd6190280f095c26182444f4574e37a
Na0 <koonayoung51@naver.com>
2021-04-13 22:16:32 +0900

    [ADD] Program 클래스 생성

---
+++ en_4 Library/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace en_4_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.ShowMenu();
+        }
+    }
+}

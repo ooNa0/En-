e38b54794b5eeb7c92663bd154f51fbac441aa09
Na0 <koonayoung51@naver.com>
2021-04-15 02:35:18 +0900

    [ADD] Check 클래스 생성

---
+++ en_4 Library/Check.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace en_4_Library
+{
+    class Check
+    {
+        public bool English(string input)
+        {
+            if (!(Regex.IsMatch(input, "^[a-zA-Z]*$")))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("영문자가 입력이 되지 않았습니다.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadLine();
+            }
+        }
+    }
+}

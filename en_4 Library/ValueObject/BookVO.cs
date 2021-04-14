6dcf5c8ee34606cfd04e47766327a199fd1df830
Na0 <koonayoung51@naver.com>
2021-04-15 03:47:08 +0900

    [ADD] VO 생성

---
+++ en_4 Library/ValueObject/BookVO.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace en_4_Library
+{
+    class BookVO
+    {
+        private string id; // 도서 번호
+        private string bookName;      // 도서명
+        private string publisher; // 출판사명
+        private string author;    // 저자명
+        private int quantity;  // 수량
+        private bool isBorrowed; // 대출중
+        private string borrowDate; // 대출일
+        private DateTime returnDate; // 반납일
+
+        public BookVO(string bookName, string publisher, string author, int quantity)
+        {
+            this.BookName = bookName;
+            this.Publisher = publisher;
+            this.Author = author;
+            this.Quantity = quantity;
+        }
+
+        public string BookName // 도서명
+        {
+            get { return bookName; }
+            set { bookName = value; }
+        }
+        public string Publisher // 출판사명
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+        public string Author // 저자명
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public int Quantity // 수량
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        public string BorrowDate
+        {
+            get { return borrowDate; }
+            set { borrowDate = value; }
+        }
+        public DateTime RorrowDate
+        {
+            get { return returnDate; }
+            set { returnDate = value; }
+        }
+        public override string ToString()
+        {
+            return BookName.PadRight(23, ' ') + Author.PadRight(20, ' ') + Publisher.PadRight(20, ' ') + Quantity;//base.ToString();
+        }
+
+        // 생성자 - 부모 클래스가 자식 클래스의 기반, 코드의 재활용
+        // 상속 - 초기화 값 많고, 생설자가 필요할 경우
+        //:this(id, bookName, publisher, "작가 미상")
+    }
+}
---
+++ en_4 Library/ValueObject/UserVO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace en_4_Library
+{
+    class UserVO
+    {
+        private string id;
+        private string password;
+        private string name;
+        private string age;
+        private string phoneNumber;
+        private int borrowBookNumber = 0;
+
+        public UserVO()
+        {
+            this.Id = id;
+            this.PassWord = password;
+            this.Name = name;
+            this.Age = age;
+            this.PhoneNumber = phoneNumber;
+        }
+
+        public string Id // 아이디
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string PassWord // 비밀번호
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Name // 이름
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Age // 나이
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string PhoneNumber // 전화번호
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public int BorrowBookNumber
+        {
+            get { return borrowBookNumber; }
+            set { borrowBookNumber = value; }
+        }
+
+        public override string ToString()
+        {
+            return "이름: " + Name.PadRight(4, ' ') + "아이디: " + Id.PadRight(10, ' ') + "나이: " + Age.PadRight(3, ' ') + "전화번호: " + PhoneNumber;
+        }
+    }
+}

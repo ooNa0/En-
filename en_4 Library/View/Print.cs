3267bc83b86d231585173e3653e85b72e96ce887
Na0 <koonayoung51@naver.com>
2021-04-15 02:02:53 +0900

    [ADD] Print 클래스 생성

---
+++ en_4 Library/View/Print.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace en_4_Library.View
+{
+    class Print
+    {
+        public void ShowMenu()
+        {
+            Console.WriteLine("▷ 로그인");
+            Console.WriteLine("▷ 회원가입");
+            Console.WriteLine("▷ 관리자 모드");
+            Console.WriteLine("▷ 종료");
+        }
+
+        public void ShowUserMenu()
+        {
+            Console.WriteLine("▷ 도서 대출");
+            Console.WriteLine("▷ 도서 반납");
+            Console.WriteLine("▷ 전체 도서 출력");
+            Console.WriteLine("▷ 도서 검색");
+            Console.WriteLine("▷ 나의 정보");
+            Console.WriteLine("▷ 로그아웃");
+        }
+
+        public void ShowAdministratorMode()
+        {
+            Console.WriteLine("▷ 도서 등록");
+            Console.WriteLine("▷ 도서 삭제");
+            Console.WriteLine("▷ 도서 검색");
+            Console.WriteLine("▷ 전체 도서 출력");
+            Console.WriteLine("▷ 회원 리스트");
+            Console.WriteLine("▷ 회원 검색");
+            Console.WriteLine("▷ 회원 삭제");
+            Console.WriteLine("▷ 돌아가기");
+        }
+
+        public void ShowHowToSearchBook()
+        {
+            Console.WriteLine("▷ 도서명");
+            Console.WriteLine("▷ 출판사");
+            Console.WriteLine("▷ 저자");
+            Console.WriteLine("▷ 돌아가기");
+        }
+        public void ShowBookList(List<BookVO> bookVO)
+        {
+            Console.Clear();
+            int count = bookVO.Count - 1;
+            Console.WriteLine("도서명               저자                 출판사명          수량 ");
+            while (count >= 0)
+            {
+                foreach (BookVO book in bookVO)
+                {
+                    Console.WriteLine("{0}", book.ToString());
+                }
+                break;
+            }
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}

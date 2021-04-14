2206d11468b8813855571b78694b02e23ec84a1a
Na0 <koonayoung51@naver.com>
2021-04-15 05:10:03 +0900

    [ADD] Menu 클래스 생성

---
+++ en_4 Library/View/Menu.cs
@@ -0,0 +1,22 @@
+﻿using en_4_Library.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace en_4_Library
+{
+    class Menu
+    {
+        public List<BookVO> book = new List<BookVO>(); // BookVO 리스트
+        public List<UserVO> user = new List<UserVO>(); // UserVO 리스트
+        Cursor cursor = new Cursor();
+        Print print = new Print();
+        public void StartMenu()
+        {
+            Console.Title = "Library - Menu";
+            //print.ShowMenu();
+            cursor.controlCursor(0, 3);
+
+        }
+    }
+}

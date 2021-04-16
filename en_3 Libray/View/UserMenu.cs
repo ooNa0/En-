f006a80361a829a2c680163dfed265df1ad23af3
Na0 <koonayoung51@naver.com>
2021-04-16 15:53:58 +0900

    [UPDATE] user 빌리는 권수 한권 제한

--- en_3 Libray/View/UserMenu.cs
+++ en_3 Libray/View/UserMenu.cs
@@ -6,42 +6,69 @@ using System.Threading;
 
 namespace en_3_Libray.View
 {
-    class UserMenu : AdministratorMenu
+    class UserMenu// : AdministratorMenu
     {
-        public void UserSelectMenu(List<UserVO> userVO, int index)
+        public void UserSelectMenu(List<UserVO> userVO, int index, List<BookVO> book)
         {
             CerserControl cerserControl = new CerserControl();
             AdministratorMenu administrator = new AdministratorMenu();
             Printing print = new Printing();
-            //UserVO userVO = new UserVO();
             Ask ask = new Ask();
             bool isFinished = true;
-           
+            string borrowBook;
+            int count;// = book.Count - 1;
+            int search;
             while (isFinished)
             {
+                search = 0;
                 print.UserMenu();
                 Console.WriteLine();
+                //DateTime now = DateTime.Now;
                 switch (cerserControl.cerserControl(0, 5))
                 {
                     case 0: // 도서 대출
-
-                        break;
-                    case 1: // 도서 반납
-                        if(userVO[index].BorrowBookNumber == 0)
+                        Console.Clear();
+                        Console.WriteLine("무슨 책을 대출하시겠습니까?");
+                        borrowBook = Console.ReadLine();
+                        count = book.Count - 1;
+                        while (count >= 0)
                         {
-                            Console.WriteLine("반납할 도서가 없습니다.");
+                            if (borrowBook == book[count].BookName)
+                            {
+                                book[count].BorrowDate = DateTime.Now.ToString("yyyyMMdd");
+                                book[count].RorrowDate = DateTime.Now.AddDays(7);
+                                book[count].Quantity -= 1;
+                                search++;
+                            }
+                            count--;
+                        }
+                        if (search == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("찾는 것이 없습니다.");
+                            Console.ForegroundColor = ConsoleColor.White;
                         }
                         else
                         {
-
+                            Console.WriteLine("대출을 완료하였습니다!");
                         }
+                        Console.WriteLine("아무 키나 눌러주세요. . .");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 1: // 도서 반납
+                        Console.WriteLine("죄송합니다.");
+                        Console.Clear();
                         break;
                     case 2: // 전체 도서 출력
                         print.BookList(book);
                         break;
                     case 3: // 도서 검색
+                        Console.Clear();
                         print.HowToSearchBook();
                         administrator.SearchBook(cerserControl.cerserControl(0, 3), book);
+
+                        Console.Clear();
                         break;
                     case 4: // 사용자 정보
                         Console.Clear();

ae63975b71ac0d6cfe46ed9a0c032dd3df64982b
Na0 <koonayoung51@naver.com>
2021-04-21 21:28:27 +0900

    [ADD] Constant 클래스 생성

---
+++ en_5 LectureTimeTable/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace en_5_LectureTimeTable
+{
+    class Constants
+    {
+        public const int CONSOLE_SIZE_X = 80;
+
+        public const int CONSOLE_SIZE_Y = 45;
+    }
+}

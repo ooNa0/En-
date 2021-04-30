4a3d96295038f88b97baa23f47873a00787c47ef
Na0 <koonayoung51@naver.com>
2021-04-30 17:05:19 +0900

    [RENAME] 메소드 이름 변경

---
+++ en_5 LectureTimeTable/LectureTimeTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace en_5_LectureTimeTable
+{
+    class LectureTimeTable
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Set();
+        }
+    }
+}

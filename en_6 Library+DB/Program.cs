e18e7b258485e407992570a9b73250dd05a2ff9b
Na0 <koonayoung51@naver.com>
2021-05-02 00:44:39 +0900

    [ADD] Library 프로젝트 생성

---
+++ en_6 Library+DB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace en_6_Library_DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

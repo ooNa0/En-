fe5116459fa43a356f95a2bf5126c3da3e268517
Na0 <koonayoung51@naver.com>
2021-05-04 02:59:37 +0900

    [UPDATE] 디자인 수정

--- en_6 Library+DB/View/OutputMenu.cs
+++ en_6 Library+DB/View/OutputMenu.cs
@@ -4,32 +4,35 @@ using System.Text;
 
 namespace en_6_Library_DB
 {
-    class PrintMenu
+    class OutputMenu
     {
-        public void Library()
+        public void LibraryDefault()
         {
             Console.WriteLine("┌──────────────────────────────────────────────────────────────────────┐");
             Console.WriteLine("│                                                                      │");
-            Console.WriteLine("│   ■       ■  ■■■■   ■■■■     ■■   ■■■■   ■    ■    │");
-            Console.WriteLine("│   ■       ■  ■     ■  ■     ■  ■    ■ ■     ■  ■    ■    │");
-            Console.WriteLine("│   ■       ■  ■■■■   ■■■■   ■■■■ ■■■■    ■  ■     │");
-            Console.WriteLine("│   ■       ■  ■     ■  ■■■     ■    ■ ■■■        ■       │");
-            Console.WriteLine("│   ■       ■  ■     ■  ■   ■■  ■    ■ ■   ■■     ■       │");
-            Console.WriteLine("│   ■■■■ ■  ■■■■   ■     ■  ■    ■ ■     ■     ■       │");
+            Console.WriteLine("│            ■           ■  ■    ■            ■           ■    │");
+            Console.WriteLine("│   ■■■■ ■   ■■■  ■  ■■■■  ■■■■  ■ ■■■■  □□ □□          │");
+            Console.WriteLine("│         ■ ■■■     ■■  ■    ■        ■■■       ■  □  □   □  │");
+            Console.WriteLine("│   ■■■■ ■  ■     ■■  ■■■■  ■■■■  ■ ■■■    □  □□□        │");
+            Console.WriteLine("│   ■       ■   ■■■  ■            ■        ■ ■        □  □   □│");
+            Console.WriteLine("│   ■■■■ ■           ■ ■■■■■ ■■■■  ■  ■■■■ □□ □□               │");
             Console.WriteLine("│                                                                      │");
             Console.WriteLine("└──────────────────────────────────────────────────────────────────────┘");
             Console.WriteLine(); Console.WriteLine();
         }
         public void ShowFirstMenu()
         {
+            //Console.SetWindowSize(Constant.CONSOLE_SIZE_X, Constant.CONSOLE_SIZE_Y);
+            LibraryDefault();
+            Console.WriteLine("                              0. 종료\n");
             Console.WriteLine("                              1. 로그인\n");
             Console.WriteLine("                              2. 회원가입\n");
-            Console.WriteLine("                              3. 관리자 모드\n");
-            Console.WriteLine("                              4. 종료");
+            Console.WriteLine("                              3. 관리자 모드");
         }
 
         public void ShowUserMenu()
         {
+            LibraryDefault();
             Console.WriteLine("                              0. 로그아웃\n");
             Console.WriteLine("                              1. 도서 대출\n");
             Console.WriteLine("                              2. 도서 반납\n");
@@ -40,11 +43,12 @@ namespace en_6_Library_DB
 
         public void ShowAdministratorMenu()
         {
+            LibraryDefault();
             Console.WriteLine("                              0. 돌아가기\n");
             Console.WriteLine("                              1. 도서 등록\n");
             Console.WriteLine("                              2. 도서 삭제\n");
-            Console.WriteLine("                              3. 도서 검색\n");
-            Console.WriteLine("                              4. 전체 도서 출력\n");
+            Console.WriteLine("                              3. 전체 도서 출력\n");
+            Console.WriteLine("                              4. 도서 검색\n");
             Console.WriteLine("                              5. 회원 리스트\n");
             Console.WriteLine("                              6, 회원 검색\n");
             Console.WriteLine("                              7. 회원 삭제");
@@ -52,6 +56,7 @@ namespace en_6_Library_DB
 
         public void ShowHowToSearchBook()
         {
+            LibraryDefault();
             Console.WriteLine("                              0. 돌아가기\n");
             Console.WriteLine("                              1. 도서명\n");
             Console.WriteLine("                              2. 출판사\n");

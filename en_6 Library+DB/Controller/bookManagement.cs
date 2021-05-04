87e6b2edc158f547a80bdc07302bb04a380e723e
Na0 <koonayoung51@naver.com>
2021-05-05 08:51:22 +0900

    [FIX] 책 삭제 오류 수정

---
+++ en_6 Library+DB/Controller/bookManagement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace en_6_Library_DB
+{
+    class bookManagement
+    {
+        private OutputData outputData;
+        private OutputMenu outputMenu;
+        private InputManagement inputManagement;
+        private DataAccessObject dataAccessObject;
+        public bookManagement(OutputMenu outputMenu, OutputData outputData, InputManagement inputManagement, DataAccessObject dataAccessObject)
+        {
+            this.outputData = outputData;
+            this.outputMenu = outputMenu;
+            this.inputManagement = inputManagement;
+            this.dataAccessObject = dataAccessObject;
+        }
+
+        public void SearchBook()
+        {
+
+            outputMenu.ShowHowToSearchBook();
+            switch (inputManagement.GetMenuNumber(Constant.WHAT_SEARCH_MENU_NUMBER))
+            {
+                case Constant.EXIT:
+                    return;
+                case Constant.BOOK_TITLE: // 도서명
+                    outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_TITLE, inputManagement.SearchName()));
+                    Console.Write("엔터하면 뒤로갑니다.");
+                    Console.ReadLine();
+                    break;
+                case Constant.BOOK_PUBLISHER: // 출판사
+                    outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_PUBLISHER, inputManagement.SearchName()));
+                    Console.Write("엔터하면 뒤로갑니다.");
+                    Console.ReadLine(); break;
+                case Constant.BOOK_AUTHOR: // 저자
+                    outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_AUTHOR, inputManagement.SearchName()));
+                    Console.Write("엔터하면 뒤로갑니다.");
+                    Console.ReadLine(); break;
+            }
+        }
+    }
+}

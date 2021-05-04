0675d72f43108f2481fbdc5536a9e580510a3847
Na0 <koonayoung51@naver.com>
2021-05-04 21:43:49 +0900

    [SET] 창 사이즈 조절

--- en_6 Library+DB/View/OutputMenu.cs
+++ en_6 Library+DB/View/OutputMenu.cs
@@ -8,59 +8,60 @@ namespace en_6_Library_DB
     {
         public void LibraryDefault()
         {
-            Console.WriteLine("┌──────────────────────────────────────────────────────────────────────┐");
-            Console.WriteLine("│                                                                      │");
-            Console.WriteLine("│            ■           ■  ■    ■            ■           ■    │");
-            Console.WriteLine("│   ■■■■ ■   ■■■  ■  ■■■■  ■■■■  ■ ■■■■  □□ □□          │");
-            Console.WriteLine("│         ■ ■■■     ■■  ■    ■        ■■■       ■  □  □   □  │");
-            Console.WriteLine("│   ■■■■ ■  ■     ■■  ■■■■  ■■■■  ■ ■■■    □  □□□        │");
-            Console.WriteLine("│   ■       ■   ■■■  ■            ■        ■ ■        □  □   □│");
-            Console.WriteLine("│   ■■■■ ■           ■ ■■■■■ ■■■■  ■  ■■■■ □□ □□               │");
-            Console.WriteLine("│                                                                      │");
-            Console.WriteLine("└──────────────────────────────────────────────────────────────────────┘");
+            Console.Clear();
+            Console.WriteLine("  ┌──────────────────────────────────────────────────────────────────────┐");
+            Console.WriteLine("  │                                                                      │");
+            Console.WriteLine("  │            ■           ■  ■    ■            ■                   │");
+            Console.WriteLine("  │   ■■■■ ■   ■■■  ■  ■■■■  ■■■■  ■ ■■■■  □□ □□");
+            Console.WriteLine("  │         ■ ■■■     ■■  ■    ■        ■■■       ■  □  □   □");
+            Console.WriteLine("  │   ■■■■ ■  ■     ■■  ■■■■  ■■■■  ■ ■■■    □  □□□");
+            Console.WriteLine("  │   ■       ■   ■■■  ■            ■        ■ ■        □  □   □");
+            Console.WriteLine("  │   ■■■■ ■           ■ ■■■■■ ■■■■  ■  ■■■■ □□ □□");
+            Console.WriteLine("  │                                                                      │");
+            Console.WriteLine("  └──────────────────────────────────────────────────────────────────────┘");
             Console.WriteLine(); Console.WriteLine();
         }
         public void ShowFirstMenu()
         {
-            //Console.SetWindowSize(Constant.CONSOLE_SIZE_X, Constant.CONSOLE_SIZE_Y);
+            Console.SetWindowSize(Constant.CONSOLE_SIZE_X, Constant.CONSOLE_SIZE_Y);
             LibraryDefault();
-            Console.WriteLine("                              0. 종료\n");
-            Console.WriteLine("                              1. 로그인\n");
-            Console.WriteLine("                              2. 회원가입\n");
-            Console.WriteLine("                              3. 관리자 모드");
+            Console.WriteLine("                              [0] 종료\n");
+            Console.WriteLine("                              [1] 로그인\n");
+            Console.WriteLine("                              [2] 회원가입\n");
+            Console.WriteLine("                              [3] 관리자 모드");
         }
 
         public void ShowUserMenu()
         {
             LibraryDefault();
-            Console.WriteLine("                              0. 로그아웃\n");
-            Console.WriteLine("                              1. 도서 대출\n");
-            Console.WriteLine("                              2. 도서 반납\n");
-            Console.WriteLine("                              3. 전체 도서 출력\n");
-            Console.WriteLine("                              4. 도서 검색\n");
-            Console.WriteLine("                              5. 나의 정보");
+            Console.WriteLine("                              [0] 로그아웃\n");
+            Console.WriteLine("                              [1] 도서 대출\n");
+            Console.WriteLine("                              [2] 도서 반납\n");
+            Console.WriteLine("                              [3] 전체 도서 출력\n");
+            Console.WriteLine("                              [4] 도서 검색\n");
+            Console.WriteLine("                              [5] 나의 정보");
         }
 
         public void ShowAdministratorMenu()
         {
             LibraryDefault();
-            Console.WriteLine("                              0. 돌아가기\n");
-            Console.WriteLine("                              1. 도서 등록\n");
-            Console.WriteLine("                              2. 도서 삭제\n");
-            Console.WriteLine("                              3. 전체 도서 출력\n");
-            Console.WriteLine("                              4. 도서 검색\n");
-            Console.WriteLine("                              5. 회원 리스트\n");
-            Console.WriteLine("                              6, 회원 검색\n");
-            Console.WriteLine("                              7. 회원 삭제");
+            Console.WriteLine("                              [0] 돌아가기\n");
+            Console.WriteLine("                              [1] 도서 등록\n");
+            Console.WriteLine("                              [2] 도서 삭제\n");
+            Console.WriteLine("                              [3] 전체 도서 출력\n");
+            Console.WriteLine("                              [4] 도서 검색\n");
+            Console.WriteLine("                              [5] 회원 리스트\n");
+            Console.WriteLine("                              [6] 회원 검색\n");
+            Console.WriteLine("                              [7] 회원 삭제");
         }
 
         public void ShowHowToSearchBook()
         {
             LibraryDefault();
-            Console.WriteLine("                              0. 돌아가기\n");
-            Console.WriteLine("                              1. 도서명\n");
-            Console.WriteLine("                              2. 출판사\n");
-            Console.WriteLine("                              3. 저자");
+            Console.WriteLine("                              [0] 돌아가기\n");
+            Console.WriteLine("                              [1] 도서명\n");
+            Console.WriteLine("                              [2] 출판사\n");
+            Console.WriteLine("                              [3] 저자");
         }
     }
 }

014a93594f642de9737ebaaa875bbdec9a28a7b8
Na0 <koonayoung51@naver.com>
2021-05-06 23:40:02 +0900

    [RENAME] ApiNaver 클래스 이름 변경

--- en_6 Library+DB/Model/NaverAPIAccessObject.cs
+++ en_6 Library+DB/Model/NaverAPIAccessObject.cs
@@ -3,32 +3,32 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading;
 
 namespace en_6_Library_DB
 {
     class NaverAPIAccessObject
     {
-        //https://openapi.naver.com/v1/search/book.json
+        private OutputData outputData;
+        public NaverAPIAccessObject(OutputData outputData)
+        {
+            this.outputData = outputData;
+        }
         public void ShowNaverBookList(string query)
         {
-            string url = "https://openapi.naver.com/v1/search/blog?query=" + query; // 결과가 JSON 포맷
+            string url = "https://openapi.naver.com/v1/search/book.json?query=" + query; // 결과가 JSON 포맷
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Headers.Add("X-Naver-Client-Id", "YOUR-CLIENT-ID"); // 클라이언트 아이디
-            request.Headers.Add("X-Naver-Client-Secret", "YOUR-CLIENT-SECRET");       // 클라이언트 시크릿
+            // 한글을 URL에 추가하기 위해 UTF-8 형식으로 URL 인코딩
+            request.Headers.Add("X-Naver-Client-Id", Constant.NAVER_API_CLIENT_ID); // 클라이언트 아이디
+            request.Headers.Add("X-Naver-Client-Secret", Constant.NAVER_API_CLIENT_SECRET);       // 클라이언트 시크릿
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            string status = response.StatusCode.ToString();
-            if (status == "OK")
-            {
-                Stream stream = response.GetResponseStream();
-                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-                string text = reader.ReadToEnd();
-                Console.WriteLine(text);
-            }
-            else
-            {
-                Console.WriteLine("Error 발생=" + status);
-            }
-        }
-    
+
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            outputData.ShowAllNaverBookList(reader);
+            string text = reader.ReadToEnd();
+            Console.WriteLine(text);
+            Thread.Sleep(10000);
+        }    
     }
 }

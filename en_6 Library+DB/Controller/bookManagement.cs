25f0c19ee74dba2da5dd81eae7238759404e99af
Na0 <koonayoung51@naver.com>
2021-05-07 15:34:59 +0900

    [REMOVE] 불필요 안내문 삭제

--- en_6 Library+DB/Controller/bookManagement.cs
+++ en_6 Library+DB/Controller/bookManagement.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace en_6_Library_DB
 {
-    class bookManagement
+    class BookManagement
     {
         private OutputData outputData;
         private OutputMenu outputMenu;
         private InputManagement inputManagement;
         private DataAccessObject dataAccessObject;
-        public bookManagement(OutputMenu outputMenu, OutputData outputData, InputManagement inputManagement, DataAccessObject dataAccessObject)
+        public BookManagement(OutputMenu outputMenu, OutputData outputData, InputManagement inputManagement, DataAccessObject dataAccessObject)
         {
             this.outputData = outputData;
             this.outputMenu = outputMenu;
@@ -18,28 +18,31 @@ namespace en_6_Library_DB
             this.dataAccessObject = dataAccessObject;
         }
 
-        public void SearchBook()
+        public int SearchBook()
         {
 
             outputMenu.ShowHowToSearchBook();
-            switch (inputManagement.GetMenuNumber(Constant.WHAT_SEARCH_MENU_NUMBER))
+            switch (inputManagement.GetMenuNumber(Constant.SEARCH_MENU_NUMBER))
             {
                 case Constant.EXIT:
-                    return;
+                    return 0;
                 case Constant.BOOK_TITLE: // 도서명
-                    outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_TITLE, inputManagement.SearchName()));
-                    Console.Write("엔터하면 뒤로갑니다.");
-                    Console.ReadLine();
+                    return outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_TITLE, inputManagement.SearchName()));
+                    //Console.Write("엔터하면 뒤로갑니다.");
+                    //Console.ReadLine();
                     break;
                 case Constant.BOOK_PUBLISHER: // 출판사
-                    outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_PUBLISHER, inputManagement.SearchName()));
-                    Console.Write("엔터하면 뒤로갑니다.");
-                    Console.ReadLine(); break;
+                    return outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_PUBLISHER, inputManagement.SearchName()));
+                    //Console.Write("엔터하면 뒤로갑니다.");
+                    //Console.ReadLine(); 
+                    break;
                 case Constant.BOOK_AUTHOR: // 저자
-                    outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_AUTHOR, inputManagement.SearchName()));
-                    Console.Write("엔터하면 뒤로갑니다.");
-                    Console.ReadLine(); break;
+                    return outputData.ShowAllBookList(dataAccessObject.SearchDataList(Constant.BOOK_TABLE, Constant.BOOK_COLUMN_AUTHOR, inputManagement.SearchName()));
+                    //Console.Write("엔터하면 뒤로갑니다.");
+                    //Console.ReadLine(); 
+                    break;
             }
+            return 0;
         }
     }
 }

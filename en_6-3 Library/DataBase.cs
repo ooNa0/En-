af1f1925dec3611f0d785aa30c8453c52fff1b56
Na0 <koonayoung51@naver.com>
2021-05-10 01:53:50 +0900

    [ADD] DataBase 싱글톤 적용하고싶다

---
+++ en_6-3 Library/DataBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace en_6_Library_DB
+{
+    public class DataBase
+    {/*
+        private static Instance;
+        private DataBase() { }
+        private static DataBase Instance()
+        {
+            if(Instance == null)
+            {
+                Instance = new AccessDataBase();
+            }
+        }
+        public void AccessDataBase()
+        {
+
+        }*/
+    }
+}

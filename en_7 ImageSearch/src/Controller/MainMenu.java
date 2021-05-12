7184727a190e1eec10ea6f085a7e6ab96d00b9d5
Na0 <koonayoung51@naver.com>
2021-05-12 22:24:40 +0900

    [SET] 생성자 사용

--- en_7 ImageSearch/src/Controller/MainMenu.java
+++ en_7 ImageSearch/src/Controller/MainMenu.java
@@ -1,12 +1,23 @@
 package Controller;
 
-import javax.swing.*;
+//import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 @SuppressWarnings("serial")
 
 
 public class MainMenu extends JFrame {
+	private JPanel panel;
+	private JButton imageSearchButton;
+	private JButton searchHistoryButton;
 	public MainMenu() {
-		
+		panel = new JPanel(); //패널을 생성
+		imageSearchButton = new JButton("이미지 서치");
+		searchHistoryButton = new JButton("검색 내역 보기");
 	}
 	
 	public void startMenu() {
@@ -20,7 +31,6 @@ public class MainMenu extends JFrame {
 	}
 	
 	private void createPanel() {
-		JPanel panel = new JPanel(); //패널을 생성
 		panel.setLayout(null); //패널의 Layout을 NULL
 		panel.setBounds(0, 0, 700, 500); //패널의 크기 및 위치 지정 (x,y로 부터 넓이(width, 높이(height))
 	  
@@ -28,14 +38,14 @@ public class MainMenu extends JFrame {
 	    //testArea.setBounds(50, 50, 300, 300); //JTeatArea 크기 및 위치 지정
 	    //testArea.setEditable(false); //실행시 JtextArea edit 금지 (글을 쓸 수 없음) true면 가능
 
-		JButton a = new JButton("이미지 서치");  //버튼 생성
-		JButton b = new JButton("검색 내역 보기");
-		a.setBounds(70, 250, 200, 70); //크기 지정
-		b.setBounds(400, 250, 200, 70);
+		//JButton imageSearchButton = new JButton("이미지 서치");  //버튼 생성
+		//JButton searchHistoryButton = new JButton("검색 내역 보기");
+		imageSearchButton.setBounds(70, 250, 200, 70); //크기 지정
+		searchHistoryButton.setBounds(400, 250, 200, 70);
 	  
   	    //panel.add(testArea); //패널에 TextArea add
-	    panel.add(a); //패널에 버튼 add
-	    panel.add(b); 
+	    panel.add(imageSearchButton); //패널에 버튼 add
+	    panel.add(searchHistoryButton); 
 	    this.add(panel);  //Frame에 Panel add (JFrame을 상속받았기에 this(생략가능)가 JFrame)
 	}
 }

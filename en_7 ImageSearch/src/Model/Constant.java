17e6db0e22ba12b4a61527764dcbdf69a9added7
Na0 <koonayoung51@naver.com>
2021-05-14 23:28:49 +0900

    [ADD] Constant

---
+++ en_7 ImageSearch/src/Model/Constant.java
@@ -0,0 +1,25 @@
+package Model;
+
+public class Constant {
+	public static String KAKAO_API_KEY = "KakaoAK 4d74071d3fbd89dfcb53c6ab3e6dd5e0";
+	
+	public static String KAKAO_API_LINK = "https://dapi.kakao.com/v2/search/image.json?query=";
+	public static String KAKAO_API_SIZE = "&&size=";
+
+	public static String ENCODE_KOREAN = "UTF-8";
+	public static String IMAGESEARCH_BUTTON = "이미지 찾기";
+	public static String SHOWRECORD_BUTTON = "검색 내역 보기";
+	public static String LABEL_SEARCHITEM_INFORMATION = "검색할 것을 입력해주세요 :";
+	public static String LABEL_SHOWDATABASERECORD_INFORMATION = "검색 기록 보기";
+	
+	public static Integer PANEL_COLOR_GREY_SET = 140;
+	public static Integer BUTTON_HORIZONTAL_SIZE = 200;
+	public static Integer BUTTON_VERTICAL_SIZE = 70;
+	public static Integer BUTTON_HORIZONTAL_POSITION = 250;
+	public static Integer FRIST_BUTTON_VERTICAL_POSITION = 70;
+	public static Integer SECOND_BUTTON_VERTICAL_POSITION = 400;
+	public static Integer FRAME_HORIZONTAL_SIZE = 700;
+	public static Integer FRAME_VERTICAL_SIZE = 500;
+	
+	
+}//vertical

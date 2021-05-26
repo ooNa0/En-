c759ba06deb22ac85ffea9aeb5504383d722325b
Na0 <koonayoung51@naver.com>
2021-05-26 17:07:15 +0900

    [UPDATE] 코드 간략화 (swirch case에 한줄만)

--- en_8 Calculator_9401/src/Model/CalculateManagement.java
+++ en_8 Calculator_9401/src/Model/CalculateManagement.java
@@ -25,17 +25,11 @@ public class CalculateManagement{
 		return calculatorString;
 	}
 	
-	public String arithmetic(int operator, BigDecimal firstOperand, BigDecimal secondOperand, JTextField inputNumberField) {
+	public BigDecimal arithmetic(int operator, BigDecimal firstOperand, BigDecimal secondOperand, JTextField inputNumberField) {
 		switch (operator) { // Constant.SUBSTRACT_NUMBER 하면 case expressions must be constant expressions에러뜸 ㅠ
-		case 1:
-			//inputNumberField.setText(String.valueOf(new DecimalFormat("#,###,###,###,###.################").format(firstOperand.add(secondOperand))));//new DecimalFormat("#,###").format(
-			return String.valueOf(firstOperand.add(secondOperand));
-		case 2:
-			//inputNumberField.setText(String.valueOf(new DecimalFormat("#,###,###,###,###.################").format((firstOperand.subtract(secondOperand)))));//new DecimalFormat("#,###").format
-			return String.valueOf(firstOperand.subtract(secondOperand));
-		case 3:
-			//inputNumberField.setText(String.valueOf(new DecimalFormat("#,###,###,###,###.################").format(((firstOperand.multiply(secondOperand)).setScale(14, RoundingMode.HALF_UP)).stripTrailingZeros())));//new DecimalFormat("#,###").format
-			return String.valueOf((firstOperand.multiply(secondOperand)).stripTrailingZeros());//.setScale(14, RoundingMode.HALF_UP)
+		case 1: return firstOperand.add(secondOperand);//String.valueOf(
+		case 2: return firstOperand.subtract(secondOperand);//String.valueOf(
+		case 3: return (firstOperand.multiply(secondOperand));
 		case 4:
 			System.out.println("first = " + firstOperand);
 			System.out.println(secondOperand);
@@ -46,10 +40,8 @@ public class CalculateManagement{
 				inputNumberField.setText("0으로 나눌 수 없습니다.");return null;		
 			}
 			else{
-				//inputNumberField.setText(String.valueOf((firstOperand.divide(secondOperand, MathContext.DECIMAL64))));//new DecimalFormat("#,###.0").format
-				return String.valueOf(new DecimalFormat("#,###,###,###,###.################").format(firstOperand.divide(secondOperand, MathContext.DECIMAL64)));}
-		}//new DecimalFormat("###,###.##").format(new BigDecimal(inputNumberField.getText().replaceAll("\\,", "")
+				return firstOperand.divide(secondOperand, MathContext.DECIMAL64);}
+		}
 		return null;
 	}
-//(new DecimalFormat("###,###.##").format(firstOperand.divide(secondOperand, MathContext.DECIMAL64)));}
 }

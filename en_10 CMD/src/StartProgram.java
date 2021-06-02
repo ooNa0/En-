8b2908043a07dfa111e9cd348efcc0a8e6e94bc6
Na0 <koonayoung51@naver.com>
2021-06-02 23:34:13 +0900

    [ADD] StartProgram

---
+++ en_10 CMD/src/StartProgram.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+import Controller.CommandProgram;
+
+public class StartProgram {
+	public static void main(String[] args) {
+		new CommandProgram();
+	} // end-main
+}

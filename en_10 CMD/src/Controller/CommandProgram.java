8d90a54e8c6c8cb2eb3969116af20debb1d4f3cd
Na0 <koonayoung51@naver.com>
2021-06-04 14:08:54 +0900

    [ADD] cd에서 절대 경로 입력 기능 추가

--- en_10 CMD/src/Controller/CommandProgram.java
+++ en_10 CMD/src/Controller/CommandProgram.java
@@ -12,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
+import java.util.stream.Collectors;
 
 import Model.*;
 import View.InputManagement;
@@ -22,6 +22,7 @@ public class CommandProgram {
 	private ApplyCommand command;
 	private InputManagement input;
 	private ShowResult result;
+	private Exception exception = new Exception();
 	private dataManagement dataManagement = new dataManagement();
 	private String currentPath;
 
@@ -29,9 +30,10 @@ public class CommandProgram {
 		command = new ApplyCommand();
 		input = new InputManagement();
 		result = new ShowResult();
-		currentPath = System.getProperty("user.home");
+		currentPath = Constant.START_POSITION_VALUE;
 		//System.out.println(p.getCanonicalPath());
 		
+		/*
 		String pd = "C:\\Users";
 		File p = new File("C:\\Users");
 		System.out.println(pd.lastIndexOf("\\"));
@@ -40,8 +42,8 @@ public class CommandProgram {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		// showDirectory();
+		}*/
+		//result.showDirectory();
 		// clearScreen();
 		runProgram();
 
@@ -49,7 +51,8 @@ public class CommandProgram {
 
 	private void runProgram() {
 		//List<String> inputlist = new ArrayList<String>();
-		List<String> inputlist = dataManagement.splitString(input.startCommand(command.getVersion(), currentPath));
+		String inputString = input.startCommand(command.getVersion(), currentPath);
+		List<String> inputlist = dataManagement.splitString(inputString);
 		//String firstsplittedString[] = dataManagement.splitString(input.startCommand(command.getVersion()));
 		while (true) {
 			if (inputlist.isEmpty()) { }
@@ -59,7 +62,7 @@ public class CommandProgram {
 				//	break;
 				case "dir":
 					System.out.println(command.getVolume());
-					result.showDirectory();
+					result.showDirectory(currentPath);
 					break;
 				case "cls":
 					result.clearScreen();
@@ -78,13 +81,27 @@ public class CommandProgram {
 					break;
 				default:
 					if(inputlist.get(0).substring(0, 2).equals("cd")) {
+						
+						if(inputlist.size() == 1) {
+							
+						}
+						else { // 띄어쓰기가 무조건 있거
+							
+						}
 						if(inputlist.get(0).length() == 2) { // cd에 띄어쓰기
 							if(inputlist.size() == 1) { // cd만 입력
 								System.out.println(result.showCurrentPath(currentPath)); // 현재 경로 출력
 							}
 							else { // cd [값] 이런 식
 								String targetPath = null;
-								if(inputlist.get(1).contains(":")) { // cd :
+								System.out.println(inputlist.get(1));
+								System.out.println(Constant.TOP_LEVEL_PATH);
+								System.out.println(inputlist.get(1).contains(Constant.TOP_LEVEL_PATH));
+								if(inputlist.get(1).contains(Constant.TOP_LEVEL_PATH)) {
+									if(exception.isDirectoryExist(inputlist.get(1)))
+										currentPath = inputlist.get(1);
+								}
+								else if(inputlist.get(1).contains(":")) { // cd :
 									System.out.println("파일 이름, 디렉터리 이름 또는 볼륨 레이블 구문이 잘못되었습니다.");
 								}
 								else if(inputlist.get(1).equals("..") && inputlist.size() == 2) {
@@ -95,20 +112,13 @@ public class CommandProgram {
 								}
 								else {
 									targetPath = currentPath + "\\" + inputlist.get(1);
-									System.out.println(currentPath);
-									System.out.println(targetPath);
-									System.out.println(inputlist.get(1));
-								}
-								File targetFile = new File(targetPath);
-								if(targetFile.exists()) { // 파일이나 디렉토리 존재
-									if(targetFile.isFile()) {
-										System.out.println(Constant.INFORMATION_DIRECTORY_NAME_INCORRECT);
-									}
-									else {
+									//System.out.println(currentPath);
+									//System.out.println(targetPath);
+									//System.out.println(inputlist.get(1));
+									if(exception.isDirectoryExist(targetPath))
 										currentPath = targetPath;
-									}
 								}
-							}//
+							}
 						}
 						else { // cd에 띄어쓰기 안함
 							//if(inputlist.get(0).length() == 3) {// cd. 이런식
@@ -149,8 +159,8 @@ public class CommandProgram {
 								}
 								if(inputlist.get(0).substring(2, 3).equals("^")) {
 									System.out.print("More?");
-									String inputString = new Scanner(System.in).nextLine();
-									System.out.println("cd" + inputString + "뒤에 입력한 값'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는 배치 파일이 아닙니다.");
+									String calculationInputString = new Scanner(System.in).nextLine();
+									System.out.println("cd" + calculationInputString + "뒤에 입력한 값'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는 배치 파일이 아닙니다.");
 								}
 							//}
 							//else if(inputlist.get(0).length() == 4){
@@ -160,7 +170,8 @@ public class CommandProgram {
 					}
 					break;
 				}
-				inputlist = dataManagement.splitString(input.inputCommand(currentPath));
+				inputString = input.inputCommand(currentPath);
+				inputlist = dataManagement.splitString(inputString);
 				//String splittedString[] = dataManagement.splitString(input.startCommand(command.getVersion()));
 			}
 			// splittedString[] = (input.inputCommand()).split(" ");
--- en_10 CMD/src/Controller/Exception.java
+++ en_10 CMD/src/Controller/Exception.java
@@ -1,5 +1,26 @@
 package Controller;
 
-public class Exception {
+import java.io.File;
 
+import Model.Constant;
+
+public class Exception {
+	public boolean isDirectoryExist(String path) {
+
+		File targetFile = new File(path);
+		if(targetFile.exists()) { // 파일이나 디렉토리 존재
+			System.out.println(targetFile.isFile());
+			if(targetFile.isFile()) {
+				System.out.println(Constant.INFORMATION_DIRECTORY_NAME_INCORRECT);
+			}
+			else {
+				return true;//currentPath = targetPath;
+			}
+		}
+		else {
+			System.out.println("지정된 경로를 찾을 수 없습니다.");
+		}
+		
+		return false;
+	}
 }

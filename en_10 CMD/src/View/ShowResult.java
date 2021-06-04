4340dc8b01c53e82d6743ff63427ef029bbabb66
Na0 <koonayoung51@naver.com>
2021-06-04 21:12:22 +0900

    [ADD] 상위 폴더 보여지는거 추가

--- en_10 CMD/src/View/ShowResult.java
+++ en_10 CMD/src/View/ShowResult.java
@@ -1,7 +1,11 @@
 package View;
 
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
@@ -16,15 +20,22 @@ public class ShowResult {
 
 	private NumberFormat sizeFormatString = NumberFormat.getInstance(Locale.KOREA);
 	private DecimalFormat sizeFormat = new DecimalFormat("#,###");
+	// 원하는 날짜 포맷으로 값 저장
+	private SimpleDateFormat fileDateFormat = new SimpleDateFormat(Constant.DIRECTORY_DATE_FORMAT);
+	
+	public String showCurrentPath(String path) {
+		File file = new File(path);		
+		try {
+			return file.getCanonicalPath().replace("\\\\", "\\");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 	public void helpCommand() {
 		System.out.println(Constant.HELP_COMMAND_INFORMATION);
-	}
-	
-	public void copyCommand(String from, String to) {
-		
-		//파일을 복사하는 메소드를 실행시킵니다.
-		//정상처리 되었을때는 1을 실패시는 -1를 리턴하게 됩니다.
 	}
 	
 	public void clearScreen() {
@@ -33,30 +44,36 @@ public class ShowResult {
 		}
 	}
 	
-	public void showDirectory() { // /b 파일 이르므만 보게 하는거
+	public void showDirectory(String path) { // /b 파일 이르므만 보게 하는거
 		// Model 친구들이다.
 		int filesSize = 0;
 		int fileNumber = 0;
-		int directoryNumber = 0;
-		String path = System.getProperty("user.home") + "\\Desktop";
+		int directoryNumber = 2;
+		String attr = ""; // 파일의 속성
+		String size = ""; // 파일 용량
+		
+		System.out.println(path + " 디렉터리\n");		
+		
 		File file = new File(path); // 보고자 하는 디렉토리(파일)
+		
+		if(!path.equals(Constant.DIRECTORY_DATE_FORMAT)) {
+				//FileTime time = Files.readAttributes(file.toPath(), BasicFileAttributes.class).creationTime();
+				//System.out.printf("%s %5s %12s %s\n", fileDateFormat.format(time.toMillis()).toString(), attr, size, ".");
+				System.out.printf("%s %5s %12s %s\n", fileDateFormat.format(new Date(file.lastModified())).toString(), "<DIR>", size, ".");
+				System.out.printf("%s %5s %12s %s\n", fileDateFormat.format(new Date(file.lastModified())).toString(), "<DIR>", size, "..");
 
-		System.out.println(" " + file.getAbsolutePath() + " 디렉토리\n");
-
+		}
 		// 디렉토리 안의 모든 파일 목록 가져오기
 		File[] files = file.listFiles();
 		// 하위 디렉토리 정보를 저장할 리스트 생성, 파일 배열의 인텍스 저장
 		List<Integer> subDirctioryList = new ArrayList<Integer>();
-		// 원하는 날짜 포맷으로 값 저장
-		SimpleDateFormat fileDateFormat = new SimpleDateFormat(Constant.DIRECTORY_DATE_FORMAT);
 		for (int i = 0; i < files.length; i++) {
-			String attr = ""; // 파일의 속성
-			String size = ""; // 파일 용량
+			attr = ""; // 파일의 속성
+			size = ""; // 파일 용량
 
 			if (files[i].isDirectory()) {
 				attr = "<DIR>";
 				directoryNumber++;
-				// size = sizeFormat.format(new BigDecimal(files[i].length())).toString() + "";
 				subDirctioryList.add(i);
 			}
 			if (files[i].isFile()) {
@@ -69,15 +86,11 @@ public class ShowResult {
 			}
 			System.out.printf("%s %5s %12s %s\n", fileDateFormat.format(new Date(files[i].lastModified())).toString(),
 					attr, size, files[i].getName());
-		}//
-		//
-		File root = new File("C:\\");
+		}
 		System.out.printf("              %d개 파일    %16s 바이트\n", fileNumber, sizeFormatString.format(filesSize));
-		System.out.printf("              %d개 디렉토리 %16s 바이트 남음\n\n",directoryNumber, sizeFormat.format(new BigDecimal(root.getUsableSpace())));
-		
-		 //try { System.out.printf("              %d개 파일 %16s 바이트 남음\n",
-		 //directoryNumber, new byte[FileUtils.sizeOfDirectory(path)]); } catch
-		 //(IOException e) { e.printStackTrace(); }
+		System.out.printf("              %d개 디렉토리 %16s 바이트 남음\n",directoryNumber, sizeFormat.format(new BigDecimal(file.getUsableSpace())));
+		//System.out.printf("              %d개 디렉토리 %16s 바이트 남음\n\n", directoryNumber, sizeFormat.format(new BigDecimal((file.getUsableSpace()) - (root.getUsableSpace() / Math.pow(1024, 3)))));
+
 		 
 	}
 	

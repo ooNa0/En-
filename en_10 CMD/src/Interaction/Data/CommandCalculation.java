a89bb53eba67de01e00ae1dec01f2eeed59d31f1
Na0 <koonayoung51@naver.com>
2021-06-07 15:34:52 +0900

    [UPDATE] 상대경로 함수사용(아무값 누르면 터짐)

--- en_10 CMD/src/Interaction/Data/CommandCalculation.java
+++ en_10 CMD/src/Interaction/Data/CommandCalculation.java
@@ -1,4 +1,4 @@
-package Model;
+package Interaction.Data;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -7,37 +7,41 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CommandCalculation {	
-	
+public class CommandCalculation {
+
 	public String getVersion() {
 		String outVertionInformation = Constant.EMPTY_VALUE;
 		try {
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(new ProcessBuilder(Constant.EXECUTION_COMMAND).start().getInputStream()));
+			BufferedReader buffer = new BufferedReader(
+					new InputStreamReader(new ProcessBuilder(Constant.EXECUTION_COMMAND).start().getInputStream()));
 			outVertionInformation = buffer.readLine() + Constant.PROGRAM_ENTER_FORMAT;
 			outVertionInformation += buffer.readLine() + Constant.PROGRAM_ENTER_FORMAT;
 			outVertionInformation += buffer.readLine();
-		} catch (IOException e) {}
+		} catch (IOException e) {
+		}
 		return outVertionInformation;
 	}
-	
+
 	public String getVolume() {
 		String outVolumeInformation = Constant.EMPTY_VALUE;
 		try {
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(new ProcessBuilder(Constant.EXECUTION_COMMAND, "/c", "dir").start().getInputStream()));
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(
+					new ProcessBuilder(Constant.EXECUTION_COMMAND, "/c", "dir").start().getInputStream()));
 			outVolumeInformation = buffer.readLine() + Constant.PROGRAM_ENTER_FORMAT;
 			outVolumeInformation += buffer.readLine() + Constant.PROGRAM_ENTER_FORMAT;
 			outVolumeInformation += buffer.readLine();
-		} catch (IOException e) {}
+		} catch (IOException e) {
+		}
 		return outVolumeInformation;
 	}
-	
-	public void copyDirectory(String fromPath, String toPath){ // 미완
+
+	public void copyDirectory(String fromPath, String toPath) { // 미완
 		File sourceF = new File(fromPath);
 		File targetF = new File(toPath);
 		File[] target_file = sourceF.listFiles();
 		for (File file : target_file) {
 			File temp = new File(targetF.getAbsolutePath() + File.separator + file.getName());
-			if(file.isDirectory()){
+			if (file.isDirectory()) {
 				temp.mkdir();
 				copyDirectory(file.getPath(), temp.getPath());
 			} else {
@@ -45,45 +49,45 @@ public class CommandCalculation {
 				FileOutputStream fos = null;
 				try {
 					fis = new FileInputStream(file);
-					fos = new FileOutputStream(temp) ;
+					fos = new FileOutputStream(temp);
 					byte[] b = new byte[4096];
 					int cnt = 0;
-					while((cnt=fis.read(b)) != -1){
+					while ((cnt = fis.read(b)) != -1) {
 						fos.write(b, 0, cnt);
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
-				} finally{
+				} finally {
 					try {
 						fis.close();
 						fos.close();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}	
+					}
 				}
 			}
-		  }	
-	    }
-	
+		}
+	}
+
 	public boolean copy(String originalFilePath, String copyFilePath) {
 		File originalFile = new File(originalFilePath);
 		File copyFile = new File(copyFilePath);
-		
+
 		try {
-			//파일의 내용을 읽어오기위한 준비
+			// 파일의 내용을 읽어오기위한 준비
 			FileInputStream fis = new FileInputStream(originalFile);
-			
-			//파일의 내용을 쓰기 위한 준비
+
+			// 파일의 내용을 쓰기 위한 준비
 			FileOutputStream fos = new FileOutputStream(copyFile);
 
-			//파일을 읽고 쓰기를 합니다. 
+			// 파일을 읽고 쓰기를 합니다.
 			int nRealByte = 0;
 			while ((nRealByte = fis.read()) != -1) {
 				fos.write(nRealByte);
 			}
-			
-			//파일스트림 닫기
+
+			// 파일스트림 닫기
 			fis.close();
 			fos.close();
 
@@ -93,15 +97,15 @@ public class CommandCalculation {
 		return true;
 	}
 
-	//삭제하는 메소드
+	// 삭제하는 메소드
 	public void delete(String originalFilePath, String currentPath) {
 		if (!originalFilePath.contains(Constant.TOP_LEVEL_PATH)) { // 상대 경로 -> 절대 경로
 			originalFilePath = currentPath + "\\" + originalFilePath;
 		}
 		File file = new File(originalFilePath);
 		// 디렉토리이면 그아래의 파일 삭제
-		if(file.isDirectory())
-				deleteDirectoy(originalFilePath);
+		if (file.isDirectory())
+			deleteDirectoy(originalFilePath);
 		file.delete();
 
 	}

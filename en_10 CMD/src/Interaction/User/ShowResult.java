4396febe6fd1166bc65eff47774f3b70edbf175c
Na0 <koonayoung51@naver.com>
2021-06-07 20:14:00 +0900

    [REMOVE] 슬래시 처리 부분 삭제

--- en_10 CMD/src/Interaction/User/ShowResult.java
+++ en_10 CMD/src/Interaction/User/ShowResult.java
@@ -1,4 +1,4 @@
-package View;
+package Interaction.User;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,53 +14,60 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import Model.Constant;
+import Interaction.Data.CommandCalculation;
+import Interaction.Data.Constant;
 
 public class ShowResult {
 
 	private NumberFormat sizeFormatString = NumberFormat.getInstance(Locale.KOREA);
-	private DecimalFormat sizeFormat = new DecimalFormat("#,###");
-	// 원하는 날짜 포맷으로 값 저장
-	private SimpleDateFormat fileDateFormat = new SimpleDateFormat(Constant.DIRECTORY_DATE_FORMAT);
-	
-	public String showCurrentPath(String path) {
-		File file = new File(path);		
-		try {
-			return file.getCanonicalPath().replace("\\\\", "\\");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
+	private DecimalFormat sizeFormat = new DecimalFormat(Constant.INSERT_COMMAS_PATTERN);
+	private SimpleDateFormat fileDateFormat = new SimpleDateFormat(Constant.DIRECTORY_DATE_FORMAT); // 원하는 날짜 포맷으로 값 저장
+
+	private CommandCalculation command;
+	public ShowResult(CommandCalculation command) {
+		this.command = command;
 	}
-	
+
+	public void printMessage(String message) {
+		System.out.println(message);
+	}
+
 	public void helpCommand() {
 		System.out.println(Constant.HELP_COMMAND_INFORMATION);
 	}
-	
+
 	public void clearScreen() {
-		for(int i = 0; i < 50; i++) {
+		for (int index = Constant.INITIAL_SIZE; index < Constant.NEED_EMPTY_SCREEN_SIZE; index++) {
 			System.out.println();
 		}
 	}
-	
-	public void showDirectory(String path) { // /b 파일 이르므만 보게 하는거
-		// Model 친구들이다.
-		int filesSize = 0;
-		int fileNumber = 0;
-		int directoryNumber = 2;
-		String attr = ""; // 파일의 속성
-		String size = ""; // 파일 용량
+
+	public void showDirectory(String path, String currentPath) { //
+
+		System.out.println(command.getVolume());	
 		
-		System.out.println(path + " 디렉터리\n");		
+		int filesSize = Constant.INITIAL_SIZE;
+		int fileNumber = Constant.INITIAL_SIZE;
+		int directoryNumber = Constant.INITIAL_DIRECTORY_SIZE;
+		String attr; // 파일의 속성
+		String size; // 파일 용량
+
+		if(path == null) {
+			System.out.println(currentPath + Constant.DIRECTORY);
+			return;
+		}
 		
+		System.out.println(path + Constant.DIRECTORY);
+
 		File file = new File(path); // 보고자 하는 디렉토리(파일)
 		
-		if(!path.equals(Constant.DIRECTORY_DATE_FORMAT)) {
-				//FileTime time = Files.readAttributes(file.toPath(), BasicFileAttributes.class).creationTime();
-				//System.out.printf("%s %5s %12s %s\n", fileDateFormat.format(time.toMillis()).toString(), attr, size, ".");
-				System.out.printf("%s %5s %12s %s\n", fileDateFormat.format(new Date(file.lastModified())).toString(), "<DIR>", size, ".");
-				System.out.printf("%s %5s %12s %s\n", fileDateFormat.format(new Date(file.lastModified())).toString(), "<DIR>", size, "..");
+		if (!path.equals(Constant.TOP_LEVEL_PATH)) {
+			System.out.printf(Constant.DIRECTION_INFORMATION_PATTERN,
+					fileDateFormat.format(new Date(file.lastModified())).toString(), Constant.DIRECTORY_ABBREVIATION,
+					Constant.EMPTY_VALUE, Constant.PARENT_FOLDER_PATH);
+			System.out.printf(Constant.DIRECTION_INFORMATION_PATTERN,
+					fileDateFormat.format(new Date(file.lastModified())).toString(), Constant.DIRECTORY_ABBREVIATION,
+					Constant.EMPTY_VALUE, Constant.PARENT_FOLDER_RELAIVE_PATH);
 
 		}
 		// 디렉토리 안의 모든 파일 목록 가져오기
@@ -68,31 +75,28 @@ public class ShowResult {
 		// 하위 디렉토리 정보를 저장할 리스트 생성, 파일 배열의 인텍스 저장
 		List<Integer> subDirctioryList = new ArrayList<Integer>();
 		for (int i = 0; i < files.length; i++) {
-			attr = ""; // 파일의 속성
-			size = ""; // 파일 용량
+			attr = Constant.EMPTY_VALUE; // 파일의 속성
+			size = Constant.EMPTY_VALUE; // 파일 용량
 
 			if (files[i].isDirectory()) {
-				attr = "<DIR>";
+				attr = Constant.DIRECTORY_ABBREVIATION;
 				directoryNumber++;
 				subDirctioryList.add(i);
 			}
 			if (files[i].isFile()) {
-				if (files[i].getName().endsWith(".ini")) { // 얜 출력도 안함
+				if (files[i].isHidden()) { // 숨긴 파일은 넘어가기
 					continue;
 				}
 				fileNumber++;
 				filesSize += files[i].length();
-				size = sizeFormat.format(new BigDecimal(files[i].length())).toString() + "";
+				size = sizeFormat.format(new BigDecimal(files[i].length())).toString() + Constant.EMPTY_VALUE;
 			}
-			System.out.printf("%s %5s %12s %s\n", fileDateFormat.format(new Date(files[i].lastModified())).toString(),
-					attr, size, files[i].getName());
+			System.out.printf(Constant.DIRECTION_INFORMATION_PATTERN,
+					fileDateFormat.format(new Date(files[i].lastModified())).toString(), attr, size,
+					files[i].getName());
 		}
-		System.out.printf("              %d개 파일    %16s 바이트\n", fileNumber, sizeFormatString.format(filesSize));
-		System.out.printf("              %d개 디렉토리 %16s 바이트 남음\n",directoryNumber, sizeFormat.format(new BigDecimal(file.getUsableSpace())));
-		//System.out.printf("              %d개 디렉토리 %16s 바이트 남음\n\n", directoryNumber, sizeFormat.format(new BigDecimal((file.getUsableSpace()) - (root.getUsableSpace() / Math.pow(1024, 3)))));
-
-		 
+		System.out.printf(Constant.FILE_INFORMATION, fileNumber, sizeFormatString.format(filesSize));
+		System.out.printf(Constant.DIRECTORY_INFORMATION, directoryNumber,
+				sizeFormat.format(new BigDecimal(file.getUsableSpace())));
 	}
-	
-	
 }
